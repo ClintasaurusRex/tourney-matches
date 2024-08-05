6eae4b6a94352b2b3ec01ecd96c5e5264f31cad6
Clint Hiles <clinthiles1992@gmail.com>
2024-08-05 14:28:08 -0700

    imported PlayerList and MatchList to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-// import logo from './logo.svg';
 import './App.css';
+import PlayerList from './components/PlayerList';
+import MatchList  from './components/MatchList';
 
 function App() {
   return (
     <div className="App">
-      <h1>TOURNEY MATCHES!</h1>
+      <h1>\
+      {/* the {" "} ensures that there is always a trailing space after */}
+        Tourney Matches{" "}
+        <span>Where coding and Tournaments found their Match!</span>
+      </h1>
+      <PlayerList />
+      <MatchList />
     </div>
   );
 }
