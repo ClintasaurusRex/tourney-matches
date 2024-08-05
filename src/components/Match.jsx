ee6b48bebf97327149291f2aa197e3bc8f9f3bd1
Clint Hiles <clinthiles1992@gmail.com>
2024-08-05 16:21:57 -0700

    added props to Player and Match

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -8,19 +8,19 @@ function Match(props)  {
       <span>vs</span>
        {props.players[1]}
        </h1>
-      {/* To be shown when there is a winner */}
-      <h2>{props.scoreDifference}</h2>
-      {/* To be shown when there is no winner */}
-      <h2>No winners yet!</h2>
+      {props.winner && <h2>{props.winner} is the winner by
+      {props.scoreDifference}</h2>}
+
+      {!props.winner && <h2>No winners yet</h2>}
     </article>
   );
 }
-// replacing <article className="Match">
+/* // replacing <article className="Match">
     //   <h1>Joe <span>vs</span> Bob</h1>
-    //   {/* To be shown when there is a winner */}
+    //   {/* To be shown when there is a winner */
     //   <h2>Joe is the winner by 1!</h2>
     //   {/* To be shown when there is no winner */}
     //   <h2>No winners yet!</h2>
-    // </article>
+    // </article> */}
 
 export default Match;
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,17 +3,29 @@ import React from "react";
 function Player(props) {
   return (
     <article className="Player">
-      <h1>
-        {props.firstName} <span>{props.gamerTag}</span> {props.lastName}
-      </h1>
-      {/* To be shown when there are no wins  */}
-      <h2 className="zero">Currently no wins</h2>
-      {/* To be shown where there is 1 win */}
-      <h2>Currently at 1 win</h2>
-      {/* Currently to be shown when there is more than 1 win */}
-      <h2>Currently at {props.wins} wins</h2>
-    </article>
+    <h1>
+      {props.firstName} <span>{props.gamerTag}</span> {props.lastName}
+    </h1>
+    {props.wins === 0 && <h2 className="zero">Currently no wins</h2>}
+
+    {props.wins === 1 && <h2>Currently at 1 win</h2>}
+
+   {props.wins > 1 && <h2>Currently at {props.wins} wins</h2>}
+  </article>
   );
 }
 
 export default Player;
+
+
+/* <article className="Player">
+<h1>
+  {props.firstName} <span>{props.gamerTag}</span> {props.lastName}
+</h1>
+{/* To be shown when there are no wins  */
+// {<h2 className="zero">Currently no wins</h2>}
+/* To be shown where there is 1 win */
+/* <h2>Currently at 1 win</h2> */
+/* Currently to be shown when there is more than 1 win */
+/* <h2>Currently at {props.wins} wins</h2> */
+// </article> */
